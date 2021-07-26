a2e6ea941cd70e9a1b63637dcbf8c0563705494e
Duy Thanh <nguyenduythanh2349@gmail.com>
2021-07-26 17:07:09 +0700

    Change block colour

--- definition.js
+++ definition.js
@@ -1,8 +1,7 @@
 Blockly.Blocks["update_ds18b20"] = {
   init: function () {
     this.jsonInit({
-      [{
-      colour: "#ff3333",
+      colour: "#ae00ae",
       tooltip: "",
       message0: "cập nhật cảm biến ds18b20 cổng %1 pin %2",
       args0: [
@@ -37,7 +36,7 @@ Blockly.Blocks["update_ds18b20"] = {
 Blockly.Blocks["read_ds18b20"] = {
   init: function () {
     this.jsonInit({
-      colour: "#ff3333",
+      colour: "#ae00ae",
       tooltip: "",
       message0: "đọc cảm biến ds18b20 cổng %1 pin %2",
       args0: [
@@ -62,7 +61,7 @@ Blockly.Blocks["read_ds18b20"] = {
           ],
         },
         ],
-      output: "Null",
+      output: null,
       helpUrl: "",
     });
   },
@@ -73,9 +72,9 @@ Blockly.Python["update_ds18b20"] = function (block) {
   var port = block.getFieldValue("port");
   var pin = block.getFieldValue("pin");
   // TODO: Assemble Python into code variable.
-  Blockly.Python.definitions_['import_ds18b20'] = "import onewire";
-  Blockly.Python.definitions_['ds18b20' + port + pin] = "ds18b20_"+port+"_"+pin+" = onewire.DS18B20(onewire.OneWire(Pin(PORTS_DIGITAL[" + port + "][" + pin + "])))";
-  var code = "ds18b20_"+port+"_"+pin+".convert_temp()\n";
+  Blockly.Python.definitions_['import_onewire'] = "import onewire";
+  Blockly.Python.definitions_['import_ds18b20' + port + pin] = "ds18b20_"+ port +"_"+ pin +" = onewire.DS18B20(onewire.OneWire(Pin(PORTS_DIGITAL[" + port + "][" + pin + "])))";
+  var code = "ds18b20_" + port + "_" + pin + ".convert_temp()\n";
   return code;
 };
 
@@ -83,9 +82,9 @@ Blockly.Python["read_ds18b20"] = function (block) {
   var port = block.getFieldValue("port");
   var pin = block.getFieldValue("pin");
   // TODO: Assemble Python into code variable.
-  Blockly.Python.definitions_['import_ds18b20'] = "import onewire";
-  Blockly.Python.definitions_['ds18b20' + port + pin] = "ds18b20_"+port+"_"+pin+" = onewire.DS18B20(onewire.OneWire(Pin(PORTS_DIGITAL[" + port + "][" + pin + "])))";
-  var code = "ds18b20_"+port+"_"+pin+".read_temp()\n";
+  Blockly.Python.definitions_['import_onewire'] = "import onewire";
+  Blockly.Python.definitions_['import_ds18b20' + port + pin] = "ds18b20_"+ port +"_"+ pin +" = onewire.DS18B20(onewire.OneWire(Pin(PORTS_DIGITAL[" + port + "][" + pin + "])))";
+  var code = "ds18b20_" + port + "_" + pin + ".read_temp()\n";
   return [code, Blockly.Python.ORDER_NONE];
 };
 
